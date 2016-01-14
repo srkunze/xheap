3417090e97dce37db9f3a16261771939cd3aa7c4
Sven R. Kunze <srkunze@mail.de>
2016-01-14 19:37:18 +0100

    better use ascii_uppercase instead of uppercase

--- test_xheap.py
+++ test_xheap.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 import unittest
-from string import uppercase
+from string import ascii_uppercase
 
 from xheap import Heap, InvalidHeapError
 
@@ -12,12 +12,12 @@ class HeapTestCase(unittest.TestCase):
 
     def test___init__(self):
         self.assertSetEqual(set(), set(Heap()))
-        self.assertSetEqual(set(uppercase), set(Heap(uppercase)))
+        self.assertSetEqual(set(ascii_uppercase), set(Heap(ascii_uppercase)))
 
     def test_check(self):
         Heap().check()
-        Heap(uppercase).check()
-        Heap(reversed(uppercase)).check()
+        Heap(ascii_uppercase).check()
+        Heap(reversed(ascii_uppercase)).check()
 
     def test_check_invalid(self):
         heap = Heap(range(100))
@@ -26,36 +26,36 @@ class HeapTestCase(unittest.TestCase):
 
     def test_push(self):
         heap = Heap()
-        for c in reversed(uppercase):
+        for c in reversed(ascii_uppercase):
             heap.push(c)
             heap.check()
-        self.assertSetEqual(set(uppercase), set(heap))
+        self.assertSetEqual(set(ascii_uppercase), set(heap))
 
     def test_pop_first(self):
-        heap = Heap(reversed(uppercase))
+        heap = Heap(reversed(ascii_uppercase))
         sorted_items = []
-        for c in uppercase:
+        for c in ascii_uppercase:
             popped_item = heap.pop()
             heap.check()
             self.assertEqual(c, popped_item)
             sorted_items.append(popped_item)
-        self.assertSequenceEqual(uppercase, sorted_items)
+        self.assertSequenceEqual(ascii_uppercase, sorted_items)
         self.assertSetEqual(set(), set(heap))
 
     def test_pop_middle(self):
-        heap = Heap(reversed(uppercase))
+        heap = Heap(reversed(ascii_uppercase))
         self.assertEqual('M', heap.pop(13))
         self.assertEqual(25, len(heap))
         heap.check()
 
     def test_pop_last(self):
-        heap = Heap(reversed(uppercase))
+        heap = Heap(reversed(ascii_uppercase))
         self.assertEqual('U', heap.pop(25))
         self.assertEqual(25, len(heap))
         heap.check()
 
     def test_remove(self):
-        heap = Heap(reversed(uppercase))
+        heap = Heap(reversed(ascii_uppercase))
         self.assertEqual(13, heap.remove('M'))
         self.assertEqual(25, len(heap))
         heap.check()
