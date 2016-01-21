1c48823d0a23ccfd676bc989a2fdaba01a630f71
Sven R. Kunze <srkunze@mail.de>
2016-01-21 19:51:03 +0100

    split up testsuite

--- test_xheap.py
+++ test_xheap.py
@@ -2,9 +2,7 @@
 
 from __future__ import unicode_literals
 
-import heapq
 import unittest
-from contextlib import contextmanager
 from string import ascii_uppercase
 
 from xheap import Heap, InvalidHeapError, OrderHeap
@@ -66,82 +64,6 @@ class HeapTestCase(unittest.TestCase):
         self.assertRaises(NotImplementedError, heap.__setslice__, 0, 0, [])
 
 
-class HeapTimeTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.hqs = []
-        for i in range(1000):
-            hq = list(reversed(ascii_uppercase))*100
-            heapq.heapify(hq)
-            self.hqs.append(hq)
-        self.hs = []
-        for i in range(1000):
-            h = Heap(list(reversed(ascii_uppercase))*100)
-            self.hs.append(h)
-
-    def tearDown(self):
-        print('-'*40)
-
-    def test_init_time(self):
-        with print_time('heapq.init'):
-            for _ in range(1000):
-                heapq.heapify(list(reversed(ascii_uppercase)))
-        with print_time('Heap.init'):
-            for _ in range(1000):
-                Heap(reversed(ascii_uppercase))
-
-    def test_peek_time(self):
-        with print_time('heapq[0]'):
-            for _ in range(1000):
-                self.hqs[0]
-        with print_time('Heap[0]'):
-            for _ in range(1000):
-                self.hs[0]
-        with print_time('Heap.peek'):
-            for _ in range(1000):
-                self.hs[0].peek()
-
-    def test_pop_time(self):
-        with print_time('heapq.heappop'):
-            for i in range(1000):
-                heapq.heappop(self.hqs[i])
-        with print_time('Heap.pop'):
-            for i in range(1000):
-                self.hs[i].pop()
-
-    def test_push_time(self):
-        with print_time('heapq.heappush'):
-            for i in range(1000):
-                heapq.heappush(self.hqs[i], 'z')
-        with print_time('Heap.push'):
-            for i in range(1000):
-                self.hs[i].push('z')
-
-    def test_replace_time(self):
-        with print_time('heapq.heapreplace'):
-            for i in range(1000):
-                heapq.heapreplace(self.hqs[i], 'z')
-        with print_time('Heap.replace'):
-            for i in range(1000):
-                self.hs[i].replace('z')
-
-    def test_pushpop_time(self):
-        with print_time('heapq.heappushpop'):
-            for i in range(1000):
-                heapq.heappushpop(self.hqs[i], 'z')
-        with print_time('Heap.pushpop'):
-            for i in range(1000):
-                self.hs[i].pushpop('z')
-
-    def test_heapify_time(self):
-        with print_time('heapq.heapify'):
-            for i in range(1000):
-                heapq.heapify(self.hqs[i])
-        with print_time('Heap.heapify'):
-            for i in range(1000):
-                self.hs[i].heapify()
-
-
 class OrderHeapTestCase(unittest.TestCase):
 
     @staticmethod
@@ -199,12 +121,3 @@ class OrderHeapTestCase(unittest.TestCase):
     def test_setslice_not_implemented(self):
         heap = OrderHeap(reversed(ascii_uppercase), key=self.key)
         self.assertRaises(NotImplementedError, heap.__setslice__, 0, 0, [])
-
-
-@contextmanager
-def print_time(label):
-    import time
-    start = time.time()
-    yield
-    end = time.time()
-    print('{: <18} - {:8.4f}usec'.format(label, (end - start) * 1000.0))
---
+++ test_xheap_time.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import heapq
+import unittest
+from contextlib import contextmanager
+from string import ascii_uppercase
+
+from xheap import Heap
+
+
+class HeapTimeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.hqs = []
+        for i in range(1000):
+            hq = list(reversed(ascii_uppercase))*100
+            heapq.heapify(hq)
+            self.hqs.append(hq)
+        self.hs = []
+        for i in range(1000):
+            h = Heap(list(reversed(ascii_uppercase))*100)
+            self.hs.append(h)
+
+    def tearDown(self):
+        print('-'*40)
+
+    def test_init_time(self):
+        with print_time('heapq.init'):
+            for _ in range(1000):
+                heapq.heapify(list(reversed(ascii_uppercase)))
+        with print_time('Heap.init'):
+            for _ in range(1000):
+                Heap(reversed(ascii_uppercase))
+
+    def test_peek_time(self):
+        with print_time('heapq[0]'):
+            for _ in range(1000):
+                self.hqs[0]
+        with print_time('Heap[0]'):
+            for _ in range(1000):
+                self.hs[0]
+        with print_time('Heap.peek'):
+            for _ in range(1000):
+                self.hs[0].peek()
+
+    def test_pop_time(self):
+        with print_time('heapq.heappop'):
+            for i in range(1000):
+                heapq.heappop(self.hqs[i])
+        with print_time('Heap.pop'):
+            for i in range(1000):
+                self.hs[i].pop()
+
+    def test_push_time(self):
+        with print_time('heapq.heappush'):
+            for i in range(1000):
+                heapq.heappush(self.hqs[i], 'z')
+        with print_time('Heap.push'):
+            for i in range(1000):
+                self.hs[i].push('z')
+
+    def test_replace_time(self):
+        with print_time('heapq.heapreplace'):
+            for i in range(1000):
+                heapq.heapreplace(self.hqs[i], 'z')
+        with print_time('Heap.replace'):
+            for i in range(1000):
+                self.hs[i].replace('z')
+
+    def test_pushpop_time(self):
+        with print_time('heapq.heappushpop'):
+            for i in range(1000):
+                heapq.heappushpop(self.hqs[i], 'z')
+        with print_time('Heap.pushpop'):
+            for i in range(1000):
+                self.hs[i].pushpop('z')
+
+    def test_heapify_time(self):
+        with print_time('heapq.heapify'):
+            for i in range(1000):
+                heapq.heapify(self.hqs[i])
+        with print_time('Heap.heapify'):
+            for i in range(1000):
+                self.hs[i].heapify()
+
+
+@contextmanager
+def print_time(label):
+    import time
+    start = time.time()
+    yield
+    end = time.time()
+    print('{: <18} - {:8.4f}usec'.format(label, (end - start) * 1000.0))
