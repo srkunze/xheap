37e75c44994d52d7a6feb0c22c850c4e37c82f62
Sven R. Kunze <srkunze@tbz-pariv.de>
2016-03-09 19:55:33 +0100

    new version

--- xheap.py
+++ xheap.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 
 from heapq import heapify, heappushpop, heapreplace, heappop, heappush
 
-__version__ = '0.15'
-__version_info__ = (0, 15)
+__version__ = '0.16'
+__version_info__ = (0, 16)
 __all__ = ['Heap', 'OrderHeap', 'RemovalHeap', 'XHeap', 'InvalidHeapError']
 
 
